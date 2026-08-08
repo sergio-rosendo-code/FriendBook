2fd06a4a3e6d7114aa47abc6a2017a45ec6f89e9
sergio-rosendo-code <sergio.rosendo.code@protonmail.com>
2026-08-07 23:15:58 -0700

    initial project creation.

---
+++ FriendBook/FriendBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FriendBook
+//
+//  Created by Sergio Rosendo on 8/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FriendBook/FriendBook/FriendBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendBookApp.swift
+//  FriendBook
+//
+//  Created by Sergio Rosendo on 8/7/26.
+//
+
+import SwiftUI
+
+@main
+struct FriendBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
